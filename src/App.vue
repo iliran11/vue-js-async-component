1668304cb196812849ad6b6ece4b54512518392a
liran <liranc@wobi.co.il>
2017-06-28 18:00:43 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AsyncComp>
+  </AsyncComp>
+</template>
+
+<script>
+const hello = () => import('./hello.vue')
+import loader from './loader.vue'
+const AsyncComp = () => ({
+  // The component to load. Should be a Promise
+  component: hello,
+  // A component to use while the async component is loading
+  loading: loader,
+  delay: 200
+})
+
+
+export default {
+  name: 'app',
+  components: { hello, loader, AsyncComp }
+}
+</script>
---
+++ src/loader.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="loader">
+  </img>
+</template>
+
+<script>
+import loader from './assets/default.svg'
+export default {
+  data() {
+    return {
+      loader
+    }
+  }
+}
+</script>
